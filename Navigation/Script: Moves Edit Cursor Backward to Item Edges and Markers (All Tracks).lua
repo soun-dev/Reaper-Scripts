de540f45de071cea4c736c7ef11c3b967bbeefcb
soun-dev <nirmaldev@gmail.com>
2025-07-28 03:20:36 +1200

    Update Script: Moves Edit Cursor Backward to Item Edges and Markers (All Tracks).lua

--- Navigation/Script: Moves Edit Cursor Backward to Item Edges and Markers (All Tracks).lua
+++ Navigation/Script: Moves Edit Cursor Backward to Item Edges and Markers (All Tracks).lua
@@ -1,5 +1,5 @@
 -- Moves Edit Cursor Backwards to Item Edges and Markers (All Tracks)
--- Author: Nirmal Dev
+-- Author: soun-dev
 -- Description: Moves the edit cursor backwards to the next item edge (start or end) or project marker across all tracks.
 -- Version: 1.0
 
